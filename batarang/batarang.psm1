c96d54cac74852e707f547ec1c92b6a0258d27c7
Neil Roberts <neilr@just-dynamics.co.za>
2021-03-23 12:26:10 +0200

    Added Functionality Back

--- batarang/batarang.psm1
+++ batarang/batarang.psm1
@@ -1,3 +1,4 @@
 . (Join-Path $PSScriptRoot "BCModules\Register-NAVAdminToolSnapIn.ps1")
 . (Join-Path $PSScriptRoot "BCModules\Unregister-NAVAdminToolSnapIn.ps1")
 . (Join-Path $PSScriptRoot "BCModules\Get-NAVServicePath.ps1")
+. (Join-Path $PSScriptRoot "Apps\Get-NAVAppDependencies.ps1")
